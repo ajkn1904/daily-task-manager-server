dcffb1b5d2fda1a0e1cd83a2ee336fd43f2efe8d
ajkn1904 <ajknishat@gmail.com>
2022-12-28 00:26:51 +0600

    mongodb added

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 const app = express();
 
 
-
+//mongodb info
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7splzic.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
