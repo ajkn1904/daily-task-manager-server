ce68f0c9b54c70bf6ced5eaab24e78886f2482f5
ajkn1904 <ajknishat@gmail.com>
2022-12-28 00:25:31 +0600

    firebase added

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7splzic.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+client.connect(err => {
+  const collection = client.db("dailyTaskManager").collection("Users");
+  // console.log("db connected", uri)
+  
+});
+
 
 
 app.use(cors());
