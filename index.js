af786f424ede5596b7ed884ba2137af6690885cd
ajkn1904 <ajknishat@gmail.com>
2022-12-29 04:48:44 +0600

    Api for deleting task added

--- index.js
+++ index.js
@@ -65,12 +65,25 @@ async function run() {
             const updatedDoc = {
                 $set: {
                     taskName: status.taskName,
-                    description:status.description
+                    description: status.description
                 }
             }
             const result = await tasksCollection.updateOne(query, updatedDoc, option)
             res.send(result)
         })
+
+
+
+        //deleting task
+        app.delete('/dltTasks/id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const result = await tasksCollection.deleteOne(query)
+            res.send(result)
+        })
+
+
+
     }
     finally {
 
